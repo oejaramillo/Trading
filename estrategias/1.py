005cfa2ce196aba5ca7a67b58516ef05184531d8
oejaramillo <oscar.10000@hotmail.com>
2023-02-09 16:42:19 -0300

    to main

---
+++ estrategias/1.py
@@ -0,0 +1,6 @@
+# Definimos la estategia
+
+# Queremos que en cada periodo de tiempo, siempre entre al mercado, sea con una compra o con una venta
+# Esta decision dependerá únicamente de la probabildad de que en los anteriores puntos del tiempo el precio de cierr
+# haya sido mayor al precio de cierre actual
+
